defc013856bd5ad9cca76c77a06b3e897a675acf
zwl <zwl@162.com>
2023-01-10 09:28:24 +0800

    commit

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -8,5 +8,7 @@ public class Main
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
